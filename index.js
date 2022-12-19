8c8b49a57a7c268f8a5cd66f32b8dc3a40e85595
Stray Gee <stellanyambura>
2022-12-19 23:30:59 +0300

    fix bugs on html & js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 document.addEventListener("DOMContentloaded", ()=>{
-document.getElementById(Comment_form).addEventListener('submit',postComent)
+document.getElementById('Comment_form').addEventListener('submit',postComent)
 })
 function postComent(e){
 e.preventDefault(); 
-let postComent={
+let comment={
     content:e.target.input_comment.value,
 }
-postToJson(postedComment)
-
-    function postComent(postedComment){
-        return fetch('http://localhost:3000/comments',{
-          method: 'POST',
-          headers: {
-            'content-Type':'application/json',
-            Accept:'application/json',
-
-        },
-        body:JSON.stringify(postedComment)
+postToJson(comment)
+}
+ function postToJson(comment){
+ return fetch('http://localhost:3000/comments',{
+  method: 'POST',
+  headers: {
+  'content-Type':'application/json',
+  Accept:'application/json',
+},
+  body:JSON.stringify(comment)
     })
     .then(response =>response.json())
-    .then(comment =>console.log(comment))
-        }}
+    .then(data =>console.log(data))
+        }
     
